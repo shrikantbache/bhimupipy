4579856299e7770491da7f85029775034f8aa619
shrikantbache <bacheshrikant@gmail.com>
2022-08-04 07:25:53 +0530

    updated

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=['requests'],
     entry_points={
         'console_scripts': [
-            'bhimupipy=bhimupipy.__init__:start_bhimupipy',
+            'bhimupipy=bhimupipy.__init__:ExecuteBhimupiPy',
         ],
     },
 
